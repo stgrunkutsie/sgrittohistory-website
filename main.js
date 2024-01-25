014adfdadf8157dd437ebf23e31b19aca7b210d8
Ryleeeee <gregcalleri13@gmail.com>
2024-01-25 15:55:53 +0100

    DARK MODEj
    ho messo la modalita scura penso si capisca

---
+++ main.js
@@ -0,0 +1,9 @@
+function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+
+function lightMode() {
+    document.body.style.backgroundColor = "rgb(228, 228, 228)";
+    document.body.style.color = "black";
+}
